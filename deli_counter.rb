96aaa76f0625cc8a0a9d5da7cbed904a3ece1f8d
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 23:58:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ if katz_deli.length== 0
   puts "The line is currently empty."
 elsif
 
-startString = "The line is currently: "
+startString = "The line is currently:"
  i = 0 
 for i in 0..katz_deli.length-1 do
    startString += " #{i+1}. #{katz_deli[i]}"
