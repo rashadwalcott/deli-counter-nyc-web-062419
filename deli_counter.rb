daf1a6bc26b91d37c47dd9ce3da7be90a53c939f
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 23:45:33 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+if katz_deli.length== 0 
+  puts "The line is currently empty."
+  end
+
+startString = "The line is currently: "
+ i = 0 
+for i in 0..katz_deli.length-1 do
+   startString += "#{i+1}. #{katz_deli[i]} "
+ end
+ puts startString
+end
+
+
+def take_a_number(katz_deli,name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end 
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli.shift()}."
+  end
+end 
